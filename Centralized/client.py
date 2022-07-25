3a4d4f08f80cb372a60484a7e5648328a34183c3
Randy Jonathan <78534148+RandyJ149@users.noreply.github.com>
2022-07-25 22:46:02 +0530

    Update client.py
    Implemented JSON payload format and check to see if it works with the server

--- Centralized/client.py
+++ Centralized/client.py
@@ -1,10 +1,19 @@
 import socket
+import time
+import json
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('192.168.50.166', 9999))
 
-name = input("Enter the name")
-s.send(bytes(name,"utf-8"))
 
-msg = s.recv(1024)
-print(msg.decode("utf-8"))
+x = {"id":"s1", "temp":20}
+str_x = json.dumps(x)
+
+
+while True:
+    start_time = time.time()
+    s.connect(('192.168.50.166', 9999))
+    s.send(bytes(str_x,"utf-8"))
+    msg = s.recv(1024)
+    print(msg.decode("utf-8"))
+    s.close()
+    time.sleep(10 - (time.time() - start_time))
