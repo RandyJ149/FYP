8e1e19b6b42996c4d6e05606ebabbcacb2ce953d
Randy Jonathan <78534148+RandyJ149@users.noreply.github.com>
2022-07-26 22:48:09 +0530

    Client 1.1 update
    client.py code that nearly works sending data for the first time. Have to fix the error that throws up when sendind data for the second time.

--- Centralized/client.py
+++ Centralized/client.py
@@ -5,15 +5,18 @@ import json
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
-x = {"id":"s1", "temp":20}
+x = {"id":"s2", "temp":20}
 str_x = json.dumps(x)
+s.connect(('192.168.23.117', 9999))
 
 
 while True:
     start_time = time.time()
-    s.connect(('192.168.50.166', 9999))
+    # s.connect(('192.168.23.117', 9999))
+    print(s)
     s.send(bytes(str_x,"utf-8"))
     msg = s.recv(1024)
     print(msg.decode("utf-8"))
-    s.close()
-    time.sleep(10 - (time.time() - start_time))
+    # # s.close()
+    # time.sleep(10 - (time.time() - start_time))
+    time.sleep(0.1)
