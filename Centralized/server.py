a7aea6ac9b247135d05b9d60ab2c0ef1770c7f48
Randy Jonathan <78534148+RandyJ149@users.noreply.github.com>
2022-08-10 17:14:12 +0530

    Server working
    Deployed the server code on Raspberry Pi

--- Centralized/server.py
+++ Centralized/server.py
@@ -1,7 +1,9 @@
 from dataclasses import dataclass
 import socket
-from threading import *
+#from threading import *
+from _thread import *
 from queue import LifoQueue
+from urllib import response
 import pandas as pd
 import sklearn
 import joblib
@@ -15,55 +17,84 @@ s2 = LifoQueue()
 s3 = LifoQueue()
 s4 = LifoQueue()
 
+#Code for testing
+s1.put(20)
+s1.put(15)
+s1.put(20)
+s1.put(21)
+s2.put(20)
+s2.put(21)
+s2.put(15)
+s2.put(21)
+s3.put(21)
+s3.put(20)
+s3.put(20)
+s3.put(20)
+s4.put(20)
+s4.put(21)
+s4.put(21)
+s4.put(16)
 
 
-class Networking(Thread):
-	def run(self):
+
+class Networking():
+	def start(self):
 		#Declaring the stacks as Global for uploading Data
-		global s1, s2, s3, s4 
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		s.bind(('192.168.50.166', 9999))
-		s.listen(5)
-
-		while True:
-		clientsocket, address = s.accept()
-		data = clientsocket.recv(1024).decode("utf-8")
-
-		data = json.loads(data)
-		#Storing Directly into stack using eval()
-		eval(data["id"]).put(data["temp"])
-		clientsocket.send(bytes("The server received the data", "utf-8"))
-
-
-		
-
-		
-
-		#Old way of communication for testing
-		# name = clientsocket.recv(1024).decode("utf-8")
-		# name
-		# print(f"Connection from {address} has been established!")
-		# clientsocket.send(bytes("Welcome to the Server", "utf-8"))
-		clientsocket.close()
-
-
-class AnoMad(Thread):
-	def run(self):
 		global s1, s2, s3, s4
-		test = joblib.load('model.pkl')
+		ThreadCount = 0
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-		#Test Code
-		# df1 = [[1337, 20, 10.76, 21, 20.03]]
-		# df = pd.DataFrame(df1)
+		def AnoMad():
+			global s1, s2, s3, s4
+			test = joblib.load('rf.pkl')
+
+			#Test Code
+			# df1 = [[1337, 20, 10.76, 21, 20.03]]
+			# df = pd.DataFrame(df1)
+
+			while True:
+			#	start_time = time.time()
+				df1 = [[1337, s1.get(), s2.get(), s3.get(), s4.get()]]
+				df = pd.DataFrame(df1)
+
+				x = test.predict(df)
+				print(x)
+			#	time.sleep(10 - (time.time() - start_time))
+
+		start_new_thread(AnoMad, ())
+
+
+		try:
+			s.bind(('192.168.71.117', 15000))
+		except socket.error as e:
+			print(str(e))
+		print('Socket is listening..')
+
+		s.listen(10)
+
+		def multi_threaded_client(connection):
+#			connection.send(str.encode("Server is working: "))
+			connection.send(bytes("Server is working: ", "utf-8"))
+			while True:
+				data = connection.recv(2048).decode("utf-8")
+				connection.send(bytes("Received Data:", "utf-8"))
+				if not data:
+					break
+				data = json.loads(data)
+				#Storing Directly into stack using eval()
+				eval(data["id"]).put(data["temp"])
+			connection.close()
 
 		while True:
-			start_time = time.time()
-			df1 = [[s1.get(), s2.get(), s3.get(), s4.get()]]
-			df = pd.DataFrame(df1)
-
-			x = test.predict(df)
-			print(x)
-			time.sleep(10000 - ((time.time() - start_time)*1000))
-
+			clientsocket, address = s.accept()
+			print('Connected to ' + address[0] + ':' + str(address[1]))
+			start_new_thread(multi_threaded_client, (clientsocket, ))
+			ThreadCount += 1
+			print('Thread Number:' + str(ThreadCount))
+		s.close()
 
 
+
+
+t1 = Networking()
+t1.start()
