3180738604785b6d6ac07c91c03e778830bb9997
Randy Jonathan <78534148+RandyJ149@users.noreply.github.com>
2022-08-15 04:45:18 +0530

    Client v2.0
    Penultimate push, waiting for additional changes to make if any

--- Centralized/client.py
+++ Centralized/client.py
@@ -1,28 +1,36 @@
 from email import message
-from http import client
 import socket
 import time
 import json
+import datetime
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def tstap():
+    curr_time = datetime.datetime.now()
+    time_st = curr_time.timestamp()
+    date_time = datetime.datetime.fromtimestamp(time_st)
+    str_date_time = date_time.strftime("%H:%M:%S")
+    return str_date_time
 
 
 x = {"id":"s1", "temp":20}
 str_x = json.dumps(x)
-s.connect(('192.168.71.117', 15000))
+s.connect(('192.168.123.117', 15000))
 
 #Keep Track of Connection status
 connected = True
-print("connected to server")
+print("Connected to Server")
 message = s.recv(1024).decode("utf-8")
 
 while True:
     #attempt to send and receive data, otherwise reconnect
     try:
         s.send(bytes(str_x, "utf-8"))
+        print("Packet sent at {}".format(tstap())) #Printing Time at which data is sent
         message = s.recv(1024).decode("UTF-8")
         print(message)
-        time.sleep(5)
+        time.sleep(15)
     except socket.error:
         #Set Connection status and recreate socket
         connected = False
@@ -31,7 +39,7 @@ while True:
         while not connected:
             #Attempt to reconnect, otherwise sleep for 2 seconds
             try:
-                s.connect(('192.168.23.117', 9999))
+                s.connect(('192.168.123.117', 15000))
                 connected = True
                 print("re-connection successful")
             except socket.error:
